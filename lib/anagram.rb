d1f68f3b134707996c3698abb869a15aded2538c
Ni Chia <nichia@gmail.com>
2017-11-12 15:59:59 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(a_word)
+    @word = a_word
+  end
+  
+  def match(words)
+    anagrams = words.split(" ")
+    
+  end
+end
