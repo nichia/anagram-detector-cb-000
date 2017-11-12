000a6b08469ca79d30135a7db0c4efedbfc5e746
Ni Chia <nichia@gmail.com>
2017-11-12 16:05:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,9 +5,15 @@ class Anagram
   def initialize(a_word)
     @word = a_word
   end
-  
+
   def match(words)
     anagrams = words.split(" ")
-    
+    result = []
+    anagrams.each { |w| 
+      if word.sort == w.sort
+        result << w
+      end
+    end
+    result
   end
 end
